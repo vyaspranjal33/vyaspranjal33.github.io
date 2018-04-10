4cd3e6ec883605a2fdbad44519887d1a269df46c
vyaspranjal33 <vyaspranjal33@gmail.com>
2018-04-10 18:52:45 +0530

    assets

---
+++ assets/js/main.js
@@ -0,0 +1,2 @@
+jQuery(document).ready(function($){$('body').scrollspy({target:'#header',offset:400});$(window).bind('scroll',function(){if($(window).scrollTop()>50){$('#header').addClass('navbar-fixed-top');}
+else{$('#header').removeClass('navbar-fixed-top');}});$('a.scrollto').on('click',function(e){var target=this.hash;e.preventDefault();$('body').scrollTo(target,800,{offset:-70,'axis':'y',easing:'easeOutQuad'});if($('.navbar-collapse').hasClass('in')){$('.navbar-collapse').removeClass('in').addClass('collapse');}});});
